00b1d59fd32c6f81cd32cd763cdce4004bdc0d00
Oleksandr Petkov <sasha111108@gmail.com>
2023-01-19 21:31:15 +0200

    homework 9, added validators to groups and students

--- students/models.py
+++ students/models.py
@@ -1,6 +1,11 @@
 import datetime
 
 from django.db import models
+from faker import Faker
+
+from students.validators import validate_unique_email
+
+VALID_DOMAINS = ('gmail.com', 'yahoo.com', 'test.com')
 
 
 class Student(models.Model):
@@ -14,13 +19,24 @@ class Student(models.Model):
         verbose_name='Last name',
         db_column='l_name'
     )
+    age = models.PositiveIntegerField()
     birthday = models.DateField(default=datetime.date.today)
     city = models.CharField(max_length=25, null=True, blank=True)
+    email = models.EmailField(validators=[validate_unique_email])
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'students'
 
-
-
+    @classmethod
+    def generate_fake_data(cls, cnt):
+        f = Faker()
+        for _ in range(cnt):
+            s = cls()  # s = Student()
+            s.first_name = f.first_name()
+            s.last_name = f.last_name()
+            s.email = f'{s.first_name}.{s.last_name}@{f.random.choice(VALID_DOMAINS)}'
+            s.birthday = f.date_between(start_date='-65y', end_date='-18y')
+            s.age = f.random_int(min=18, max=65)
+            s.save()
