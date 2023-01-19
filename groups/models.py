639fd620d3d904e666c88e63c56471750318656f
Oleksandr Petkov <sasha111108@gmail.com>
2023-01-19 21:13:58 +0200

    change Revert "add validator to apps groups and students"
    This reverts commit a0b6fa4d0fad255e576bb3174d4a9d432bf902b1.

--- groups/models.py
+++ groups/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from groups.validators import validate_start_date
-
 
 class Group(models.Model):
     group_name = models.CharField(
@@ -9,7 +7,7 @@ class Group(models.Model):
         verbose_name='Group name',
         db_column='gr_name'
     )
-    group_start = models.DateField(validators=[validate_start_date])
+    group_start = models.DateField()
     group_description = models.TextField(null=True, blank=True, db_column='gr_descr')
 
     class Meta:
